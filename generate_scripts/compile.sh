f684c493794b516538b0d88d35937579e086d166
antoine javet <antoine@antoine.local>
2015-02-15 19:38:04 +0100

    layout changes

--- generate_scripts/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ROOT="$(dirname "$0")/.."
-FRONTEND_PATH="${ROOT}/../TiledViewer/frontend"
-
-cat \
-    "${FRONTEND_PATH}/utils.js" "${FRONTEND_PATH}/affinetransform.js" \
-    "${FRONTEND_PATH}/pinchzoom.js" "${FRONTEND_PATH}/CanvasTilesRenderer.js" \
-    "${FRONTEND_PATH}/htmlinterface.js" | jsmin > "${ROOT}/www/js/mapviewer.min.js"
