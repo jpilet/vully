e12935287c53a2107bb74a24f5c377c1e64e3133
Julien Pilet <julien.pilet@gmail.com>
2015-02-18 17:19:44 +0100

    Trie les événements par date.

--- www/js/evenements.js
+++ www/js/evenements.js
@@ -1,4 +1,13 @@
 
+function parseDate(dateAsString) {
+  var fields = dateAsString.split(".");
+  var year = parseInt(fields[2]);
+  if (year < 2000) year += 2000;
+
+  var theDate = new Date(year,fields[1]-1,fields[0]);
+  return theDate;
+}
+
 function getGroupLabel(date) {
   var fields = date.split(".");
   var months = [
@@ -26,35 +35,44 @@ function getGroupLabel(date) {
   return date;
 }
 
+function sortEntries(entries) {
+  entries.sort(function(a,b) {
+    return parseDate(a.date) - parseDate(b.date);
+  });
+}
+
+function formatItem(entry, groupDiv) {
+  groupDiv.append("<h2>" + entry.title + "</h2>");
+  
+  groupDiv.append(entry.text);
+  if (entry.linkedFile) {
+    groupDiv.append('<footer><a href="' + entry.linkedFile + '"><img class="iconSize2" src="img/icon-data-linked.png"/></a></footer>');
+  } else {
+    groupDiv.append('<hr/>');
+  }
+}
+
 function formatEntries(selector, entries) {
   var container = $(selector);
   container.html("");
   
-  var groups = {};
+  var lastGroupLabel;
+  var groupDiv;
+  
   for (var i in entries) {
     var entry = entries[i];
     
-    var key = getGroupLabel(entry.date);
-    if (!(key in groups)) {
-      groups[key] = [];
-    }
-    groups[key].push(entry);
-  }
-  
-  for (var groupName in groups) {
-    var group = groups[groupName];
-    
-    var groupDiv = $("<div class=\"block\"><h1>" + groupName + "</h1></div>");
-    for (var i in group) {
-      var entry = group[i];
-      groupDiv.append("<h2>" + entry.title + "</h2>");
-      groupDiv.append(entry.text);
-      if (entry.linkedFile) {
-        groupDiv.append('<footer><a href="' + entry.linkedFile + '"><img class="iconSize2" src="img/icon-data-linked.png"/></a></footer>');
-      } else {
-        groupDiv.append('<hr/>');
+    var groupLabel = getGroupLabel(entry.date);
+    if (!lastGroupLabel || lastGroupLabel != groupLabel) {
+      lastGroupLabel = groupLabel;
+      if (groupDiv) {
+        container.append(groupDiv);    
       }
+      groupDiv = $("<div class=\"block\"><h1>" + groupLabel + "</h1></div>");
     }
-    container.append(groupDiv);
+    formatItem(entry, groupDiv);
+  }
+  if (groupDiv) {
+    container.append(groupDiv);    
   }
 }
